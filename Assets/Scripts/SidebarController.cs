2071bc005726cc935dfc1b58d4c700848aa9f150
Matúš Adamička <matusadamicka@gmail.com>
2023-04-02 18:01:10 +0200

    adding readme to test git

--- Assets/Scripts/SidebarController.cs
+++ Assets/Scripts/SidebarController.cs
@@ -14,7 +14,12 @@ public class SidebarController : MonoBehaviour
 
     public TMP_Text verticeType;
     public TMP_Text verticeId;
-    public TMP_Text verticeCreatedDate;
+    public TMP_Text ticketCreatedDate;
+    public TMP_Text personInitials;
+
+    public GameObject personData;
+
+    public GameObject ticketData;
 
     void Update()
     {
@@ -37,11 +42,38 @@ public class SidebarController : MonoBehaviour
 
     // Open
     public void slideOut(VerticeData verticeData) {
+        if (verticeData.verticeType == VerticeType.Person) {
+            slideOutPersonSidebar(verticeData);
+        }
+        else if (verticeData.verticeType == VerticeType.Ticket) {
+            slideOutTicketSidebar(verticeData);
+        }
+    }
+
+
+    public void slideOutPersonSidebar(VerticeData verticeData) {
         sidebar.Open();
+        focusSidebar(verticeData.verticeType);
         verticeType.text = verticeData.verticeType.ToString();
         verticeId.text = "id: "+verticeData.id.ToString();
-        verticeCreatedDate.text = "created: "+verticeData.created.ToString();
-        // Debug.Log(verticeData);
+        personInitials.text = "initials: "+verticeData.name.ToString();
+        personInitials.gameObject.transform.parent.gameObject.SetActive(true);
+    }
+
+    public void slideOutTicketSidebar(VerticeData verticeData) {
+        sidebar.Open();
+        focusSidebar(verticeData.verticeType);
+        verticeType.text = verticeData.verticeType.ToString();
+        verticeId.text = "id: "+verticeData.id.ToString();
+        ticketCreatedDate.text = "created: "+verticeData.start.ToString();
+        personInitials.gameObject.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void focusSidebar(VerticeType verticeType) {
+        if (verticeType == VerticeType.Person) personData.gameObject.SetActive(true);
+        else personData.gameObject.SetActive(false);
+        if (verticeType == VerticeType.Ticket) ticketData.gameObject.SetActive(true);
+        else ticketData.gameObject.SetActive(false);
     }
 
     // Close
