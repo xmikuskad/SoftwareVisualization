1ab66128741ea360ffb900cdb09dbd24beb8bee7
Matúš Adamička <matusadamicka@gmail.com>
2023-05-08 21:59:05 +0200

    kiviat finished

--- Assets/Scripts/VisTechniques/KiviatDiagram.cs
+++ Assets/Scripts/VisTechniques/KiviatDiagram.cs
@@ -53,31 +53,80 @@ public class KiviatDiagram : MonoBehaviour
         foreach (KeyValuePair<long, VerticeData> person in allPersons)
         {
             // changes commits repos files wikis
-            List<float> metricz = new List<float>();
+            List<float> metricz = new List<float>() { 0f, 0f, 0f, 0f, 0f };
             Dictionary<VerticeType, List<VerticeData>> personRelatedVerticesDict = dataHolder.verticeWrappers[person.Value.id].GetRelatedVerticesDict();
 
-            if (personRelatedVerticesDict.ContainsKey(VerticeType.Change)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Change].Count));
-            else metricz.Add(0.0f);
-            if (personRelatedVerticesDict.ContainsKey(VerticeType.Commit)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Commit].Count));
-            else metricz.Add(0.0f);
-            if (personRelatedVerticesDict.ContainsKey(VerticeType.RepoFile)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.RepoFile].Count));
-            else metricz.Add(0.0f);
-            if (personRelatedVerticesDict.ContainsKey(VerticeType.File)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.File].Count));
-            else metricz.Add(0.0f);
-            if (personRelatedVerticesDict.ContainsKey(VerticeType.Wiki)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Wiki].Count));
-            else metricz.Add(0.0f);
+            if (personRelatedVerticesDict.ContainsKey(VerticeType.Change))
+            {
+                List<VerticeData> personRelatedChanges = personRelatedVerticesDict[VerticeType.Change];
+                metricz[0] += (personRelatedChanges.Count);
+                foreach (VerticeData personRelatedChange in personRelatedChanges)
+                {
+                    VerticeWrapper personRelatedChangeWrapper = dataHolder.verticeWrappers[personRelatedChange.id];
+                    Dictionary<VerticeType, List<VerticeData>> verticesRelatedToChange = personRelatedChangeWrapper.GetRelatedVerticesDict();
+                    if (verticesRelatedToChange.ContainsKey(VerticeType.Commit)) metricz[1] += (verticesRelatedToChange[VerticeType.Commit].Count);
+                    else metricz[1] += (0.0f);
+                    if (verticesRelatedToChange.ContainsKey(VerticeType.RepoFile)) metricz[2] += (verticesRelatedToChange[VerticeType.RepoFile].Count);
+                    else metricz[2] += (0.0f);
+                    if (verticesRelatedToChange.ContainsKey(VerticeType.File)) metricz[3] += (verticesRelatedToChange[VerticeType.File].Count);
+                    else metricz[3] += (0.0f);
+                    if (verticesRelatedToChange.ContainsKey(VerticeType.Wiki)) metricz[4] += (verticesRelatedToChange[VerticeType.Wiki].Count);
+                    else metricz[4] += (0.0f);
+                }
+            }
 
+            if (personRelatedVerticesDict.ContainsKey(VerticeType.Commit))
+            {
+                List<VerticeData> personRelatedCommits = personRelatedVerticesDict[VerticeType.Commit];
+                metricz[1] += (personRelatedCommits.Count);
+                foreach (VerticeData personRelatedCommit in personRelatedCommits)
+                {
+                    VerticeWrapper personRelatedCommitWrapper = dataHolder.verticeWrappers[personRelatedCommit.id];
+                    Dictionary<VerticeType, List<VerticeData>> verticesRelatedToCommit = personRelatedCommitWrapper.GetRelatedVerticesDict();
+                    if (verticesRelatedToCommit.ContainsKey(VerticeType.Commit)) metricz[1] += (verticesRelatedToCommit[VerticeType.Commit].Count);
+                    else metricz[1] += (0.0f);
+                    if (verticesRelatedToCommit.ContainsKey(VerticeType.RepoFile)) metricz[2] += (verticesRelatedToCommit[VerticeType.RepoFile].Count);
+                    else metricz[2] += (0.0f);
+                    if (verticesRelatedToCommit.ContainsKey(VerticeType.File)) metricz[3] += (verticesRelatedToCommit[VerticeType.File].Count);
+                    else metricz[3] += (0.0f);
+                    if (verticesRelatedToCommit.ContainsKey(VerticeType.Wiki)) metricz[4] += (verticesRelatedToCommit[VerticeType.Wiki].Count);
+                    else metricz[4] += (0.0f);
+                }
+            }
+
+            // if (personRelatedVerticesDict.ContainsKey(VerticeType.Change)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Change].Count));
+            // else metricz.Add(0.0f);
+            // if (personRelatedVerticesDict.ContainsKey(VerticeType.Commit)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Commit].Count));
+            // else metricz.Add(0.0f);
+            // if (personRelatedVerticesDict.ContainsKey(VerticeType.RepoFile)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.RepoFile].Count));
+            // else metricz.Add(0.0f);
+            // if (personRelatedVerticesDict.ContainsKey(VerticeType.File)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.File].Count));
+            // else metricz.Add(0.0f);
+            // if (personRelatedVerticesDict.ContainsKey(VerticeType.Wiki)) metricz.Add((float)Math.Sqrt(personRelatedVerticesDict[VerticeType.Wiki].Count));
+            // else metricz.Add(0.0f);
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (metricz[i] < 0.01f) metricz[i] = 0.01f;
+                else metricz[i] = (float)Math.Log(metricz[i]);
+                if (metricz[i] < 0.01f) metricz[i] = 0.01f;
+            }
 
             Debug.Log("---- for person " + person.Value.name + " ----");
+
+
             foreach (var x in metricz) Debug.Log("currently in list " + x.ToString());
-            Debug.Log("-------");
 
             float sum = 0;
             foreach (float v in metricz) sum += v;
 
-            if (sum > 0)
+            if (sum > 0.05f)
                 metrics[person.Value.id] = metricz;
             else { Debug.Log("ignoring " + person.Value.name + "cuz its all 0"); }
+
+
+            Debug.Log("-------");
+
             // metrics[person.Value.id] = new List<float>() { 1, 2, 3, 4, 5 };
         }
         return metrics;
